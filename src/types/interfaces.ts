3d05e9140aeb220022c749b44c46af421069526b
Bonnie <moller.bon@gmail.com>
2024-02-11 18:24:22 +0100

    first commit

---
+++ src/types/interfaces.ts
@@ -0,0 +1,21 @@
+// Define an interface for the note object
+export interface Note {
+    id: string;
+    username: string;
+    title: string;
+    note: string;
+}
+
+export interface ApiResponse {
+    success: boolean;
+    notes: Note[];
+}
+
+export interface ApiError {
+    message: string
+    status: number
+  }
+
+
+
+
