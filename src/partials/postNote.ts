2d0e5bb5aa61e0cd80adadc42412aa69c7b0a980
Bonnie <moller.bon@gmail.com>
2024-02-13 10:53:10 +0100

    addadd

--- src/partials/postNote.ts
+++ src/partials/postNote.ts
@@ -3,14 +3,14 @@ import baseUrl from "../API/apiConfig";
 import { Note } from "./types/interfaces";
 import { getNotes } from "./getNotes";
 
-export const btnShowNote = document.querySelector(".btn__show-note") as HTMLElement; 
 
 export async function postNote() {
   const inputNoteTitle = document.querySelector(".input__note-title") as HTMLInputElement;
   const inputNoteBody = document.querySelector(".input__note-body") as HTMLInputElement;
   const inputUserName = document.querySelector(".input__user-name") as HTMLInputElement;
   const formBtn = document.querySelector(".btn__submit") as HTMLElement;
-
+  const btnShowNote = document.querySelector(".btn__show-note") as HTMLElement; 
+  
   formBtn.addEventListener("click", async (e) => {
     e.preventDefault();
 
